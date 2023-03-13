b72195d545d96b419f8526defb64a92c9520eabe
Nail Sharipov <nailxsharipov@gmail.com>
2023-03-13 16:50:08 +0300

    init

---
+++ iShapeDebug/iShapeDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  iShapeDebugApp.swift
+//  iShapeDebug
+//
+//  Created by Nail Sharipov on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct iShapeDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
